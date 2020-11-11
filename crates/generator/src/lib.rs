55091cf42743df4608a08dd600ed37dcc0f44a16
hupeng <bitrocks.hu@gmail.com>
2020-11-11 17:51:04 +0800

    feat: re-export HashMapCodeStore

--- crates/generator/src/lib.rs
+++ crates/generator/src/lib.rs
@@ -14,3 +14,4 @@ pub mod traits;
 pub use error::Error;
 pub use generator::Generator;
 pub use syscalls::RunResult;
+pub use syscalls::{hashmap_code_store::HashMapCodeStore, GetContractCode, RunResult};
