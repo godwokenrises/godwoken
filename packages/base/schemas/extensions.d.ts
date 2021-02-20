2f0b236ddb38f65725f1745d1744835f294d555f
jjy <jjyruby@gmail.com>
2021-02-20 16:21:35 +0800

    Remove Js components

--- packages/base/schemas/extensions.d.ts
+++
@@ -1,7 +0,0 @@
-import {CanCastToArrayBuffer, CreateOptions, OutPoint} from "./godwoken";
-export class OutPointVec {
-  constructor(reader: CanCastToArrayBuffer, options?: CreateOptions);
-  validate(compatible?: boolean): void;
-  indexAt(i: Number): OutPoint;
-  length(): Number;
-}
--- packages/godwoken/README.md
+++
@@ -1,3 +0,0 @@
-# godwoken
-
-Godwoken JS Binding
