b4018003226ff0968dc29c71f89f75f889698863
jjy <jjyruby@gmail.com>
2021-03-02 11:51:03 +0800

    Implement ChainView

--- crates/store/src/chain_view.rs
+++ crates/store/src/chain_view.rs
@@ -6,23 +6,40 @@ use gw_traits::ChainStore;
 
 use crate::transaction::StoreTransaction;
 
-/// TODO implement chain view
+/// Max block hashes we can read, not included tip
+const MAX_BLOCK_HASHES_DEPTH: u64 = 256;
+
 pub struct ChainView {
     db: StoreTransaction,
-    _tip_block_hash: H256,
+    tip_block_hash: H256,
 }
 
 impl ChainView {
-    pub fn new(db: StoreTransaction, _tip_block_hash: H256) -> Self {
-        Self {
-            db,
-            _tip_block_hash,
-        }
+    pub fn new(db: StoreTransaction, tip_block_hash: H256) -> Self {
+        Self { db, tip_block_hash }
     }
 }
 
 impl ChainStore for ChainView {
     fn get_block_hash_by_number(&self, number: u64) -> Result<Option<H256>, Error> {
-        self.db.get_block_hash_by_number(number)
+        // if we can read block number from db index, we are in the main chain
+        if let Some(tip_number) = self.db.get_block_number(&self.tip_block_hash)? {
+            if !is_number_in_a_valid_range(tip_number, number) {
+                return Ok(None);
+            }
+            // so we can direct return a block hash from db index
+            return self.db.get_block_hash_by_number(number);
+        }
+
+        // we are on a forked chain
+        // since we always execute transactions based on main chain
+        // it is a bug in the current version
+        Err("shouldn't execute transaction on forked chain"
+            .to_string()
+            .into())
     }
+}
+
+fn is_number_in_a_valid_range(tip_number: u64, number: u64) -> bool {
+    number < tip_number && number >= tip_number.saturating_sub(MAX_BLOCK_HASHES_DEPTH)
 }
