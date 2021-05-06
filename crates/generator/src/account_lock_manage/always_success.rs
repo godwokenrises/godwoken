3e80d6de085ea856862b59fce9c01306aa87a99c
Xuejie Xiao <xxuejie@gmail.com>
2021-05-06 12:55:26 +0800

    feat: Refactor Secp256k1Eth signing module (#141)
    * feat: Refactor Secp256k1Eth signing module for better Ethereum compatibility
    
    * fix: clippy
    
    * fix: Use to_id as chain ID when deploying polyjuice smart contract
    
    * feat: Use chain ID in polyjuice account
    
    * feat: Add rollup chain ID processing
    
    * fix: To address should be empty when create contract (#153)
    
    * fix: Test
    
    * test: Add a new test for to address' leading zero behavior
    
    Co-authored-by: CL <classicalliu@gmail.com>

--- crates/generator/src/account_lock_manage/always_success.rs
+++ crates/generator/src/account_lock_manage/always_success.rs
@@ -1,5 +1,8 @@
 use gw_common::H256;
-use gw_types::{bytes::Bytes, packed::Signature};
+use gw_types::{
+    bytes::Bytes,
+    packed::{L2Transaction, Script, Signature},
+};
 
 use crate::error::LockAlgorithmError;
 
@@ -13,11 +16,21 @@ pub struct AlwaysSuccess;
 ///
 /// manage.register_lock_algorithm(code_hash, Box::new(AlwaysSuccess::default()));
 impl LockAlgorithm for AlwaysSuccess {
-    fn verify_signature(
+    fn verify_withdrawal_signature(
         &self,
         _lock_args: Bytes,
         _signature: Signature,
         _message: H256,
+    ) -> Result<bool, LockAlgorithmError> {
+        Ok(true)
+    }
+
+    fn verify_tx(
+        &self,
+        _rollup_type_hash: H256,
+        _sender_script: Script,
+        _receiver_script: Script,
+        _tx: L2Transaction,
     ) -> Result<bool, LockAlgorithmError> {
         Ok(true)
     }
