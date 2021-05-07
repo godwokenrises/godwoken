76d0a5f10875952e359ab34e86831538c2be262e
classicalliu <classicalliu@gmail.com>
2021-05-07 10:39:25 +0800

    feat: New parse log format for web3 indexer

--- crates/web3-indexer/src/helper.rs
+++ crates/web3-indexer/src/helper.rs
@@ -4,14 +4,13 @@ use gw_types::packed::LogItem;
 use gw_types::prelude::*;
 use std::{convert::TryInto, usize};
 
-pub const GW_LOG_SUDT_OPERATION: u8 = 0x0;
-pub const GW_LOG_POLYJUICE_SYSTEM: u8 = 0x1;
-pub const GW_LOG_POLYJUICE_USER: u8 = 0x2;
-pub const SUDT_OPERATION_TRANSFER: u8 = 0x0;
+pub const GW_LOG_SUDT_TRANSFER: u8 = 0x0;
+pub const GW_LOG_SUDT_PAY_FEE: u8 = 0x1;
+pub const GW_LOG_POLYJUICE_SYSTEM: u8 = 0x2;
+pub const GW_LOG_POLYJUICE_USER: u8 = 0x3;
 #[derive(Default, Debug)]
 pub struct PolyjuiceArgs {
     pub is_create: bool,
-    pub is_static: bool,
     pub gas_limit: u64,
     pub gas_price: u128,
     pub value: u128,
@@ -19,18 +18,16 @@ pub struct PolyjuiceArgs {
 }
 
 impl PolyjuiceArgs {
+    // https://github.com/nervosnetwork/godwoken-polyjuice/blob/v0.6.0-rc1/polyjuice-tests/src/helper.rs#L322
     pub fn decode(args: &[u8]) -> anyhow::Result<Self> {
-        // https://github.com/nervosnetwork/godwoken-polyjuice/blob/4c9f13d7b89c4e6b833fd90ca68e972d2a7b60f0/polyjuice-tests/src/helper.rs#L183
-        let is_create = args[0] == 3u8;
-        let is_static = args[1] == 1u8;
-        let gas_limit = u64::from_le_bytes(args[2..10].try_into()?);
-        let gas_price = u128::from_le_bytes(args[10..26].try_into()?);
-        let value = u128::from_be_bytes(args[42..58].try_into()?);
-        let input_size = u32::from_le_bytes(args[58..62].try_into()?);
-        let input: Vec<u8> = args[62..(62 + input_size as usize)].to_vec();
+        let is_create = args[7] == 3u8;
+        let gas_limit = u64::from_le_bytes(args[8..16].try_into()?);
+        let gas_price = u128::from_le_bytes(args[16..32].try_into()?);
+        let value = u128::from_be_bytes(args[32..48].try_into()?);
+        let input_size = u32::from_le_bytes(args[48..52].try_into()?);
+        let input: Vec<u8> = args[52..(52 + input_size as usize)].to_vec();
         Ok(PolyjuiceArgs {
             is_create,
-            is_static,
             gas_limit,
             gas_price,
             value,
@@ -39,9 +36,11 @@ impl PolyjuiceArgs {
     }
 }
 
-pub fn account_id_to_eth_address(id: u32) -> Vec<u8> {
-    let mut data = vec![0u8; 20];
-    data[0..4].copy_from_slice(&id.to_le_bytes()[..]);
+pub fn account_id_to_eth_address(account_script_hash: H256, id: u32, ethabi: bool) -> Vec<u8> {
+    let offset = if ethabi { 12 } else { 0 };
+    let mut data = vec![0u8; offset + 20];
+    data[offset..offset + 16].copy_from_slice(&account_script_hash.as_slice()[0..16]);
+    data[offset + 16..offset + 20].copy_from_slice(&id.to_le_bytes()[..]);
     data
 }
 
@@ -51,6 +50,12 @@ pub enum GwLog {
         sudt_id: u32,
         from_id: u32,
         to_id: u32,
+        amount: u128,
+    },
+    SudtPayFee {
+        sudt_id: u32,
+        from_id: u32,
+        block_producer_id: u32,
         amount: u128,
     },
     PolyjuiceSystem {
@@ -66,35 +71,48 @@ pub enum GwLog {
     },
 }
 
+fn parse_sudt_log_data(data: &[u8]) -> (u32, u32, u128) {
+    let mut u32_bytes = [0u8; 4];
+    u32_bytes.copy_from_slice(&data[0..4]);
+    let from_id = u32::from_le_bytes(u32_bytes);
+
+    u32_bytes.copy_from_slice(&data[4..8]);
+    let to_id = u32::from_le_bytes(u32_bytes);
+
+    let mut u128_bytes = [0u8; 16];
+    u128_bytes.copy_from_slice(&data[8..24]);
+    let amount = u128::from_le_bytes(u128_bytes);
+    (from_id, to_id, amount)
+}
+
 pub fn parse_log(item: &LogItem) -> Result<GwLog> {
     let service_flag: u8 = item.service_flag().into();
     let raw_data = item.data().raw_data();
     let data = raw_data.as_ref();
     match service_flag {
-        GW_LOG_SUDT_OPERATION => {
-            if data[0] != SUDT_OPERATION_TRANSFER {
-                return Err(anyhow!("Not a sudt transfer prefix: {}", data[1]));
-            }
+        GW_LOG_SUDT_TRANSFER => {
             let sudt_id: u32 = item.account_id().unpack();
-            if data.len() != (1 + 4 + 4 + 16) {
+            if data.len() != (4 + 4 + 16) {
                 return Err(anyhow!("Invalid data length: {}", data.len()));
             }
-            let data = &data[1..];
-
-            let mut u32_bytes = [0u8; 4];
-            u32_bytes.copy_from_slice(&data[0..4]);
-            let from_id = u32::from_le_bytes(u32_bytes);
-
-            u32_bytes.copy_from_slice(&data[4..8]);
-            let to_id = u32::from_le_bytes(u32_bytes);
-
-            let mut u128_bytes = [0u8; 16];
-            u128_bytes.copy_from_slice(&data[8..24]);
-            let amount = u128::from_le_bytes(u128_bytes);
+            let (from_id, to_id, amount) = parse_sudt_log_data(data);
             Ok(GwLog::SudtTransfer {
                 sudt_id,
                 from_id,
                 to_id,
+                amount,
+            })
+        }
+        GW_LOG_SUDT_PAY_FEE => {
+            let sudt_id: u32 = item.account_id().unpack();
+            if data.len() != (4 + 4 + 16) {
+                return Err(anyhow!("Invalid data length: {}", data.len()));
+            }
+            let (from_id, block_producer_id, amount) = parse_sudt_log_data(data);
+            Ok(GwLog::SudtPayFee {
+                sudt_id,
+                from_id,
+                block_producer_id,
                 amount,
             })
         }
