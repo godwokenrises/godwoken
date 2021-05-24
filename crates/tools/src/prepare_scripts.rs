3998b2a1b824ed693b832d1ec1ceacafe8d5ac9c
EthanYuan <yuanchengxing@hotmail.com>
2021-05-24 13:14:23 +0800

    Refactoring git checkout command and run command in target dir.

--- crates/tools/src/prepare_scripts.rs
+++ crates/tools/src/prepare_scripts.rs
@@ -64,41 +64,47 @@ fn copy_scripts(_prebuild_image: &PathBuf, _scripts_dir: &Path) {}
 fn generate_script_deploy_config(_output_path: &Path) {}
 
 fn build_godwoken_scripts(repo_url: Url, root_dir: &Path, repo_name: &str) -> Result<()> {
-    run_pull_code(repo_url, root_dir, repo_name)?;
+    run_pull_code(repo_url, true, root_dir, repo_name)?;
 
     let mut target_dir = PathBuf::new();
     target_dir.push(root_dir);
     target_dir.push(repo_name);
     target_dir.push("c");
 
-    let working_dir = env::current_dir().expect("Get working dir failed");
-    env::set_current_dir(&target_dir).expect("Set target dir failed");
-    run_command("make", vec!["-w"]).expect("Run make failed");
+    run_command("make", vec!["-C", &target_dir.display().to_string()]).expect("Run make failed");
 
-    env::set_current_dir("../").expect("Set target dir failed");
-    run_command("capsule", vec!["build", "--release", "--debug-output"])
-        .expect("Run capsule build failed");
-    env::set_current_dir(&working_dir).expect("Set working dir failed");
+    target_dir.pop();
+    run_command_in_dir(
+        "capsule",
+        vec!["build", "--release", "--debug-output"],
+        &target_dir,
+    )
+    .expect("Run capsule build failed");
 
     Ok(())
 }
 
 fn build_godwoken_polyjuice(repo_url: Url, root_dir: &Path, repo_name: &str) -> Result<()> {
-    run_pull_code(repo_url, root_dir, repo_name)?;
+    run_pull_code(repo_url, true, root_dir, repo_name)?;
     Ok(())
 }
 
 fn build_clerkb(repo_url: Url, root_dir: &Path, repo_name: &str) -> Result<()> {
-    run_pull_code(repo_url, root_dir, repo_name)?;
+    run_pull_code(repo_url, true, root_dir, repo_name)?;
     Ok(())
 }
 
-fn run_pull_code(mut repo: Url, root_dir: &Path, repo_name: &str) -> Result<()> {
+fn run_pull_code(
+    mut repo: Url,
+    is_recursive: bool,
+    root_dir: &Path,
+    repo_name: &str,
+) -> Result<()> {
     log::info!("Pull code of {} ...", repo_name);
 
     let commit = repo
         .fragment()
-        .ok_or_else(|| anyhow::anyhow!("Invalid commit"))?
+        .ok_or_else(|| anyhow::anyhow!("Invalid branch, commit, or tags."))?
         .to_owned();
     repo.set_fragment(None);
 
@@ -111,33 +117,45 @@ fn run_pull_code(mut repo: Url, root_dir: &Path, repo_name: &str) -> Result<()> 
     }
     run_command("rm", vec!["-rf", &target_dir.display().to_string()]).expect("Run rm dir failed");
     fs::create_dir_all(&target_dir).expect("Create dir failed");
-    run_git_clone(repo, true, &target_dir).expect("Run git clone failed");
+    run_git_clone(repo, is_recursive, &target_dir).expect("Run git clone failed");
     run_git_checkout(&target_dir, &commit).expect("Run git checkout failed");
     Ok(())
 }
 
 fn run_git_clone(repo_url: Url, is_recursive: bool, path: &Path) -> Result<()> {
-    let arg_recursive = if is_recursive { "--recursive" } else { "" };
-    run_command(
-        "git",
-        vec![
-            "clone",
-            arg_recursive,
-            repo_url.as_str(),
-            &path.display().to_string(),
-        ],
-    )
+    let path = path.display().to_string();
+    let mut args = vec!["clone", repo_url.as_str(), path.as_str()];
+    if is_recursive {
+        args.push("--recursive");
+    }
+    run_command("git", args)
 }
 
 fn run_git_checkout(repo_relative_path: &Path, commit: &str) -> Result<()> {
-    let current_dir = env::current_dir().expect("Get current dir failed");
-    env::set_current_dir(&repo_relative_path).expect("Set current dir failed");
-    let result = run_command("git", vec!["checkout", commit]);
-    env::set_current_dir(&current_dir).expect("Set current dir failed");
+    let git_dir = format!(
+        "--git-dir={}/.git",
+        repo_relative_path.display().to_string()
+    );
+    let work_tree = format!("--work-tree={}", repo_relative_path.display().to_string());
+    run_command(
+        "git",
+        vec![git_dir.as_ref(), work_tree.as_ref(), "checkout", commit],
+    )
+}
+
+fn run_command_in_dir<I, S>(bin: &str, args: I, target_dir: &Path) -> Result<()>
+where
+    I: IntoIterator<Item = S> + std::fmt::Debug,
+    S: AsRef<OsStr>,
+{
+    let working_dir = env::current_dir().expect("Get working dir failed");
+    env::set_current_dir(&target_dir).expect("Set target dir failed");
+    let result = run_command(bin, args);
+    env::set_current_dir(&working_dir).expect("Set working dir failed");
     result
 }
 
-pub fn run_command<I, S>(bin: &str, args: I) -> Result<()>
+fn run_command<I, S>(bin: &str, args: I) -> Result<()>
 where
     I: IntoIterator<Item = S> + std::fmt::Debug,
     S: AsRef<OsStr>,
