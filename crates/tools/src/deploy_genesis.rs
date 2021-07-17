50c3110a89d8af96ad1b43ea3a3c29d0d86e8f26
EthanYuan <yuanchengxing@hotmail.com>
2021-07-17 08:19:58 +0800

    Update the err description of burn lock hash check.

--- crates/tools/src/deploy_genesis.rs
+++ crates/tools/src/deploy_genesis.rs
@@ -271,8 +271,9 @@ pub fn deploy_genesis(
         let burn_lock_script_hash: [u8; 32] = burn_lock_script.calc_script_hash().unpack();
         if H256(burn_lock_script_hash) != user_rollup_config.burn_lock_hash {
             return Err(format!(
-                "burn lock hash is not {} as expected, which is calculated by all zero burn lock script",
-                hex::encode(burn_lock_script_hash)
+                "The burn lock hash: 0x{} is not default, we suggest to use default burn lock\
+                (code_hash: 0x, hash_type: Data, args: empty)",
+                hex::encode(user_rollup_config.burn_lock_hash)
             ));
         }
         if poa_setup.round_intervals == 0 {
