60c97f991fbd77e8bf8117a8ffaed9f3eeb34055
EthanYuan <yuanchengxing@hotmail.com>
2021-07-17 17:01:31 +0800

    Impl issue bad challenge.

--- crates/tests/src/system_tests/bad_challenge.rs
+++ crates/tests/src/system_tests/bad_challenge.rs
@@ -1,20 +1,16 @@
 use crate::system_tests::test_mode_control::TestModeRpc;
-use gw_jsonrpc_types::test_mode::{ChallengeType, ShouldProduceBlock};
+use gw_jsonrpc_types::test_mode::ChallengeType;
 
 pub fn issue_bad_challenge(block_number: u64) -> Result<(), String> {
     log::info!("[test mode contro]: issue bad challenge");
     let mut test_mode_rpc = TestModeRpc::new();
-    let mut i = 0;
-    while i < 1 {
-        let ret = test_mode_rpc.should_produce_block()?;
-        if let ShouldProduceBlock::Yes = ret {
-            test_mode_rpc.issue_challenge(block_number, 0, ChallengeType::TxSignature)?;
-            i += 1;
-            log::info!(
-                "issue challenge: block number {}, target_index 0, ChallengeType TxExecution",
-                block_number
-            );
-        }
-    }
+    let challenge_type = ChallengeType::TxSignature;
+    test_mode_rpc.issue_challenge(block_number, 0, challenge_type)?;
+    log::info!(
+        "issue challenge: block number {}, target_index 0, ChallengeType {:?}",
+        block_number,
+        challenge_type
+    );
+
     Ok(())
 }
