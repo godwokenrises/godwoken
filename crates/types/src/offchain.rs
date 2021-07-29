ccff6341382deb1c2bf421a4817f21ed0bd8453d
zero.qn <zero.qn@outlook.com>
2021-07-29 14:14:03 +0800

    fix: cancel polyjuice tx challenge failure
    Save SYS_LOAD_ACCOUNT_SCRIPT script into RunResult, we can then
    package these scripts into cancel challenge witness.

--- crates/types/src/offchain.rs
+++ crates/types/src/offchain.rs
@@ -1,6 +1,6 @@
 use crate::packed::LogItem;
 use sparse_merkle_tree::H256;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone, Default)]
 pub struct RunResult {
@@ -9,6 +9,7 @@ pub struct RunResult {
     pub return_data: Vec<u8>,
     pub account_count: Option<u32>,
     pub new_scripts: HashMap<H256, Vec<u8>>,
+    pub get_scripts: HashSet<Vec<u8>>,
     pub write_data: HashMap<H256, Vec<u8>>,
     // data hash -> data full size
     pub read_data: HashMap<H256, usize>,
