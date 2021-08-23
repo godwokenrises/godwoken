b5d460885e0ea59b139852e0f6eb95cda99b0256
zero.qn <zero.qn@outlook.com>
2021-08-23 16:36:25 +0800

    format code

--- crates/types/src/offchain/mod.rs
+++ crates/types/src/offchain/mod.rs
@@ -1,10 +1,10 @@
 mod extension;
+mod pool;
 mod rollup_context;
 mod rpc;
 mod run_result;
-mod pool;
 
+pub use pool::*;
 pub use rollup_context::*;
 pub use rpc::*;
 pub use run_result::*;
-pub use pool::*;
