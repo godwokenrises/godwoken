2481d6518e48b8ffde3a7a51929561baaf6bd64c
jjy <jjyruby@gmail.com>
2021-09-11 13:49:05 +0800

    chore: optimize log of ChainEvent

--- crates/block-producer/src/types.rs
+++ crates/block-producer/src/types.rs
@@ -1,4 +1,9 @@
-use gw_types::packed::{Block, NumberHash};
+use std::fmt::{self, Display};
+
+use gw_types::{
+    packed::{Block, NumberHash},
+    prelude::Unpack,
+};
 
 #[derive(Clone)]
 pub struct SignatureEntry {
@@ -15,4 +20,37 @@ pub enum ChainEvent {
         old_tip: NumberHash,
         new_block: Block,
     },
+}
+
+impl Display for ChainEvent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::NewBlock { block } => {
+                write!(f, "ChainEvent::NewBlock{{")?;
+                write!(
+                    f,
+                    "block: <number: {}, hash: {}>",
+                    block.header().raw().number().unpack(),
+                    hex::encode(block.header().hash())
+                )?;
+                write!(f, "}}")
+            }
+            Self::Reverted { old_tip, new_block } => {
+                write!(f, "ChainEvent::Reverted{{")?;
+                write!(
+                    f,
+                    "old_tip: <number: {}, hash: {}>",
+                    old_tip.number().unpack(),
+                    old_tip.block_hash()
+                )?;
+                write!(
+                    f,
+                    "new_block: <number: {}, hash: {}>",
+                    new_block.header().raw().number().unpack(),
+                    hex::encode(new_block.header().hash())
+                )?;
+                write!(f, "}}")
+            }
+        }
+    }
 }
