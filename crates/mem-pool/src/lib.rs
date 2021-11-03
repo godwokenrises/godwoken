5c140ca333c96d1eaa68c2b04e948e52cb40d37a
zero.qn <zero.qn@outlook.com>
2021-11-03 14:14:13 +0800

    refactor(mem-pool): MemPoolBatch to push tx and withdrawal

--- crates/mem-pool/src/lib.rs
+++ crates/mem-pool/src/lib.rs
@@ -3,12 +3,12 @@
 //! MemPool only do basic verification on l2transactions & withdrawal requests,
 //! the block producer need to verify the fully verification itself.
 
+pub mod batch;
 mod constants;
 pub mod custodian;
 pub mod default_provider;
 mod deposit;
 mod mem_block;
-pub mod poller;
 pub mod pool;
 pub mod traits;
 mod types;
