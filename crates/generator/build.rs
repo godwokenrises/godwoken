13673ac1b7e518f79cbe32b03e35b6b8f7ce5f68
jjy <jjyruby@gmail.com>
2021-11-08 11:27:05 +0800

    feat: support non-x86 environment

---
+++ crates/generator/build.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+fn main() {
+    let target_family = env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    let is_windows = target_family == "windows";
+    let is_unix = target_family == "unix";
+    let is_x86_64 = target_arch == "x86_64";
+    let can_enable_asm = is_x86_64 && (is_windows || is_unix);
+
+    if cfg!(feature = "asm") && (!can_enable_asm) {
+        panic!("asm feature can only be enabled on x86_64 Linux, macOS and Windows platforms!");
+    }
+
+    if cfg!(any(feature = "asm", feature = "detect-asm")) && can_enable_asm {
+        println!("cargo:rustc-cfg=has_asm");
+    }
+}
