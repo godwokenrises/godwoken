ea007bcabaac697d30a82dd814e6d8e3bed19a0e
zero.qn <zero.qn@outlook.com>
2021-11-08 17:46:17 +0800

    fix(store): mem pool smt return under layer value on deleted key

--- crates/store/src/smt/mem_pool_smt_store.rs
+++ crates/store/src/smt/mem_pool_smt_store.rs
@@ -44,38 +44,50 @@ impl<'a> MemPoolSMTStore<'a> {
 impl<'a> Store<H256> for MemPoolSMTStore<'a> {
     fn get_branch(&self, branch_key: &BranchKey) -> Result<Option<BranchNode>, SMTError> {
         let branch_key: packed::SMTBranchKey = branch_key.pack();
-        let opt = self
+        let node_slice = match self
             .inner_store()
             .get(self.mem_pool_columns.branch_col, branch_key.as_slice())
-            .filter(|slice| slice.as_ref() != [DELETED_FLAG])
-            .or_else(|| {
-                self.inner_store()
-                    .get(self.under_layer_columns.branch_col, branch_key.as_slice())
-            })
-            .map(|slice| {
-                let branch = packed::SMTBranchNodeReader::from_slice_should_be_ok(slice.as_ref());
-                branch.to_entity().unpack()
-            });
-        Ok(opt)
+        {
+            Some(slice) if slice.as_ref() == [DELETED_FLAG] => return Ok(None),
+            Some(slice) => slice,
+            None => match self
+                .inner_store()
+                .get(self.under_layer_columns.branch_col, branch_key.as_slice())
+            {
+                Some(slice) if slice.as_ref() == [DELETED_FLAG] => return Ok(None),
+                Some(slice) => slice,
+                None => return Ok(None),
+            },
+        };
+
+        let branch = packed::SMTBranchNodeReader::from_slice_should_be_ok(node_slice.as_ref());
+        Ok(Some(branch.to_entity().unpack()))
     }
 
     fn get_leaf(&self, leaf_key: &H256) -> Result<Option<H256>, SMTError> {
-        match self
-            .store
+        let leaf_slice = match self
+            .inner_store()
             .get(self.mem_pool_columns.leaf_col, leaf_key.as_slice())
-            .filter(|slice| slice.as_ref() != [DELETED_FLAG])
-            .or_else(|| {
-                self.store
-                    .get(self.under_layer_columns.leaf_col, leaf_key.as_slice())
-            }) {
-            Some(slice) if 32 == slice.len() => {
-                let mut leaf = [0u8; 32];
-                leaf.copy_from_slice(slice.as_ref());
-                Ok(Some(H256::from(leaf)))
-            }
-            Some(_) => Err(SMTError::Store("get corrupted leaf".to_string())),
-            None => Ok(None),
+        {
+            Some(slice) if slice.as_ref() == [DELETED_FLAG] => return Ok(None),
+            Some(slice) => slice,
+            None => match self
+                .inner_store()
+                .get(self.under_layer_columns.leaf_col, leaf_key.as_slice())
+            {
+                Some(slice) if slice.as_ref() == [DELETED_FLAG] => return Ok(None),
+                Some(slice) => slice,
+                None => return Ok(None),
+            },
+        };
+
+        if 32 != leaf_slice.len() {
+            return Err(SMTError::Store("get crrupted leaf".to_string()));
         }
+
+        let mut leaf = [0u8; 32];
+        leaf.copy_from_slice(leaf_slice.as_ref());
+        Ok(Some(H256::from(leaf)))
     }
 
     fn insert_branch(&mut self, branch_key: BranchKey, branch: BranchNode) -> Result<(), SMTError> {
