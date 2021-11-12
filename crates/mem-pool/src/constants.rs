5819b2e12f03841ded941cfe94b854f2a88d1398
jjy <jjyruby@gmail.com>
2021-11-12 19:25:49 +0800

    perf: decrease MAX_MEM_BLOCK_TXS to avoid state-validator OOM

--- crates/mem-pool/src/constants.rs
+++ crates/mem-pool/src/constants.rs
@@ -3,7 +3,7 @@ pub const MAX_MEM_BLOCK_DEPOSITS: usize = 50;
 /// MAX withdrawals in the mem block
 pub const MAX_MEM_BLOCK_WITHDRAWALS: usize = 50;
 /// MAX withdrawals in the mem block
-pub const MAX_MEM_BLOCK_TXS: usize = 2500;
+pub const MAX_MEM_BLOCK_TXS: usize = 2000;
 /// MAX tx size 50 KB
 pub const MAX_TX_SIZE: usize = 50_000;
 /// MAX withdrawal size 50 KB
