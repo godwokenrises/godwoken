9c3778ce71ecebd41d77383bfe85be8eb1db30dc
zero.qn <zero.qn@outlook.com>
2021-11-19 19:25:15 +0800

    feat(rpc): dump mem block api

--- crates/config/src/config.rs
+++ crates/config/src/config.rs
@@ -37,6 +37,7 @@ pub struct Config {
 #[serde(rename_all = "lowercase")]
 pub enum RPCMethods {
     PProf,
+    DumpMemBlock,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
