4df6dda3675c94a15cdf937732668e600e3b4236
jjy <jjyruby@gmail.com>
2021-12-01 15:20:38 +0800

    fix: increase MEM_BLOCK limit

--- crates/mem-pool/src/constants.rs
+++ crates/mem-pool/src/constants.rs
@@ -1,9 +1,9 @@
 /// MAX deposits in the mem block
-pub const MAX_MEM_BLOCK_DEPOSITS: usize = 30;
+pub const MAX_MEM_BLOCK_DEPOSITS: usize = 50;
 /// MAX withdrawals in the mem block
 pub const MAX_MEM_BLOCK_WITHDRAWALS: usize = 50;
 /// MAX withdrawals in the mem block
-pub const MAX_MEM_BLOCK_TXS: usize = 500;
+pub const MAX_MEM_BLOCK_TXS: usize = 1000;
 /// MAX tx size 50 KB
 pub const MAX_TX_SIZE: usize = 50_000;
 /// MAX withdrawal size 50 KB
