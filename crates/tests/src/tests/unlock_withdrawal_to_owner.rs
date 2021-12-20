67fc38090b47a8b312b9b50fd05eb018990e6aa9
zero.qn <zero.qn@outlook.com>
2021-12-20 22:04:25 +0800

    test(unlock withdrawal): refactor push withdrawal test

--- crates/tests/src/tests/unlock_withdrawal_to_owner.rs
+++ crates/tests/src/tests/unlock_withdrawal_to_owner.rs
@@ -1,27 +1,22 @@
 #![allow(clippy::mutable_key_type)]
 
-use std::collections::HashSet;
-use std::net::SocketAddr;
+use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
 use std::time::Duration;
 
 use crate::testing_tool::chain::{
-    build_sync_tx, chain_generator, construct_block, setup_chain, ALWAYS_SUCCESS_CODE_HASH,
-    ALWAYS_SUCCESS_PROGRAM, WITHDRAWAL_LOCK_PROGRAM,
+    build_sync_tx, construct_block, setup_chain, ALWAYS_SUCCESS_CODE_HASH, ALWAYS_SUCCESS_PROGRAM,
+    WITHDRAWAL_LOCK_PROGRAM,
 };
 use crate::testing_tool::mem_pool_provider::DummyMemPoolProvider;
 use crate::testing_tool::verify_tx::{verify_tx, TxWithContext};
 
-use async_jsonrpc_client::{HttpClient, Output, Params, Transport};
 use async_trait::async_trait;
 use ckb_types::prelude::{Builder, Entity};
-use gw_block_producer::test_mode_control::TestModeControl;
 use gw_block_producer::withdrawal_unlocker::BuildUnlockWithdrawalToOwner;
 use gw_chain::chain::{L1Action, L1ActionContext, SyncParam};
 use gw_common::H256;
-use gw_config::{BlockProducerConfig, NodeMode};
-use gw_rpc_client::rpc_client::RPCClient;
-use gw_rpc_server::registry::{Registry, RegistryArgs};
-use gw_rpc_server::server::start_jsonrpc_server;
+use gw_config::BlockProducerConfig;
 use gw_types::core::ScriptHashType;
 use gw_types::offchain::{CellInfo, CollectedCustodianCells, InputCellInfo, RollupContext};
 use gw_types::packed::{
@@ -31,12 +26,10 @@ use gw_types::packed::{
 };
 use gw_types::prelude::Pack;
 use gw_utils::transaction_skeleton::TransactionSkeleton;
-use serde_json::json;
 
 const CKB: u64 = 100000000;
 
 #[test]
-#[ignore]
 fn test_push_withdrawal_with_owner_lock() {
     let _ = env_logger::builder().is_test(true).try_init();
 
@@ -45,7 +38,7 @@ fn test_push_withdrawal_with_owner_lock() {
     let rollup_cell = CellOutput::new_builder()
         .type_(Some(rollup_type_script.clone()).pack())
         .build();
-    let mut chain = setup_chain(rollup_type_script.clone());
+    let mut chain = setup_chain(rollup_type_script);
 
     // Deposit 2 accounts
     const DEPOSIT_CAPACITY: u64 = 1000000 * CKB;
@@ -84,7 +77,7 @@ fn test_push_withdrawal_with_owner_lock() {
     chain.sync(param).unwrap();
     assert!(chain.last_sync_event().is_success());
 
-    // Generate random withdrawals, deposits, txs
+    // Generate random withdrawals
     const WITHDRAWAL_CAPACITY: u64 = 1000 * CKB;
     let alice = accounts.first().unwrap().to_owned();
     let withdrawal: WithdrawalRequestExtra = {
@@ -128,74 +121,14 @@ fn test_push_withdrawal_with_owner_lock() {
         };
         mem_pool.set_provider(Box::new(provider));
         mem_pool.reset_mem_block().unwrap();
+
+        mem_pool
+            .push_withdrawal_request(withdrawal.clone())
+            .unwrap();
+        mem_pool
+            .push_withdrawal_request(withdrawal_with_owner_lock.clone())
+            .unwrap();
     }
-
-    let rpc_addr = random_bindable_socket_addr();
-    let _rpc_server = {
-        let store = chain.store().clone();
-        let mem_pool = chain.mem_pool().clone();
-        let generator = chain_generator(&chain, rollup_type_script.clone());
-        let rollup_config = generator.rollup_context().rollup_config.to_owned();
-        let rollup_context = generator.rollup_context().to_owned();
-        let rpc_client = {
-            let indexer_client =
-                HttpClient::new(format!("http://{}", random_bindable_socket_addr())).unwrap();
-            let ckb_client =
-                HttpClient::new(format!("http://{}", random_bindable_socket_addr())).unwrap();
-            let rollup_type_script =
-                ckb_types::packed::Script::new_unchecked(rollup_type_script.as_bytes());
-            RPCClient::new(
-                rollup_type_script,
-                rollup_context,
-                ckb_client,
-                indexer_client,
-            )
-        };
-
-        let args: RegistryArgs<TestModeControl> = RegistryArgs {
-            store,
-            mem_pool,
-            generator,
-            tests_rpc_impl: None,
-            rollup_config,
-            mem_pool_config: Default::default(),
-            node_mode: NodeMode::FullNode,
-            rpc_client,
-            send_tx_rate_limit: Default::default(),
-            server_config: Default::default(),
-            fee_config: Default::default(),
-        };
-
-        smol::spawn(start_jsonrpc_server(
-            rpc_addr.parse().unwrap(),
-            Registry::new(args),
-        ))
-        .detach();
-    };
-
-    let godwoken_rpc_client = HttpClient::new(format!("http://{}", rpc_addr)).unwrap();
-    // let resp = smol::block_on(godwoken_rpc_client.request("gw_get_tip_block_hash", None)).unwrap();
-    // assert!(matches!(resp, Output::Success(_)), "get tip block hash");
-
-    let resp = smol::block_on(godwoken_rpc_client.request(
-        "gw_submit_withdrawal_request_2",
-        Some(Params::Array(vec![json!(format!(
-            "0x{}",
-            hex::encode(withdrawal.as_bytes())
-        ))])),
-    ))
-    .unwrap();
-    assert!(matches!(resp, Output::Success(_)), "submit withdrawal");
-
-    let resp = smol::block_on(godwoken_rpc_client.request(
-        "gw_submit_withdrawal_request_2",
-        Some(Params::Array(vec![json!(format!(
-            "0x{}",
-            hex::encode(withdrawal_with_owner_lock.as_bytes())
-        ))])),
-    ))
-    .unwrap();
-    assert!(matches!(resp, Output::Success(_)), "submit withdrawal");
 
     // Check restore withdrawals, deposits and txs
     {
@@ -221,14 +154,19 @@ fn test_push_withdrawal_with_owner_lock() {
     };
 
     assert_eq!(block_result.block.withdrawals().len(), 2);
-    assert_eq!(
-        block_result.withdrawal_extras.first().unwrap().as_slice(),
-        withdrawal.as_slice()
-    );
-    assert_eq!(
-        block_result.withdrawal_extras.last().unwrap().as_slice(),
-        withdrawal_with_owner_lock.as_slice()
-    );
+
+    let expected_withdrawals: HashMap<[u8; 32], _> = HashMap::from_iter([
+        (withdrawal.hash(), withdrawal),
+        (
+            withdrawal_with_owner_lock.hash(),
+            withdrawal_with_owner_lock,
+        ),
+    ]);
+
+    for extra in block_result.withdrawal_extras.iter() {
+        let expected = expected_withdrawals.get(&extra.hash()).unwrap();
+        assert_eq!(extra.as_slice(), expected.as_slice());
+    }
 }
 
 #[test]
@@ -425,20 +363,4 @@ fn random_always_success_script(opt_rollup_script_hash: Option<&H256>) -> Script
             args.pack()
         })
         .build()
-}
-
-fn random_bindable_socket_addr() -> String {
-    let mut count = 20;
-    let socket = socket2::Socket::new(socket2::Domain::IPV4, socket2::Type::STREAM, None).unwrap();
-
-    while count > 0 {
-        let random_port = (rand::random::<u32>() + 1024) % 65534;
-        let addr = format!("127.0.0.1:{}", random_port);
-        if let Ok(()) = socket.bind(&addr.parse::<SocketAddr>().unwrap().into()) {
-            return addr;
-        }
-        count -= 1;
-    }
-
-    panic!("no random bindable address");
 }
