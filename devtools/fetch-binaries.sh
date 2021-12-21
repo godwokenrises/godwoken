f467dc13155bddc7ffb439ca86ca85ccb5ac6e24
zero.qn <zero.qn@outlook.com>
2021-12-21 21:15:26 +0800

    chore(devtools): pull godwoken-prebuilds support unlock withdrawal

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -4,4 +4,7 @@ docker pull nervos/godwoken-prebuilds:latest
 [ -d $dir_name ] && rm -rf $dir_name && echo "Delete old dir"
 mkdir -p $dir_name && echo "Create dir"
 docker run -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:latest cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
+# TODO: Wait prebuilds update
+docker pull ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner
+docker run -v $(pwd)/$dir_name:/tmp zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
 echo "Done"
