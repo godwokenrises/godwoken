4b06f8a50b623626736fa05ebd1fff6b3fcb06b3
zero.qn <zero.qn@outlook.com>
2021-12-22 14:34:29 +0800

    fix(devtools): ghcr.io images id

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -6,5 +6,5 @@ mkdir -p $dir_name && echo "Create dir"
 docker run -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:latest cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
 # TODO: Wait prebuilds update
 docker pull ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner
-docker run -v $(pwd)/$dir_name:/tmp zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
+docker run -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
 echo "Done"
