53408a68a85f196d51eceea3bc9c12b02a779aea
zero.qn <zero.qn@outlook.com>
2021-12-24 15:54:29 +0800

    refactor(block-producer): remove Transaction clone

--- crates/block-producer/src/utils.rs
+++ crates/block-producer/src/utils.rs
@@ -3,10 +3,10 @@ use gw_rpc_client::rpc_client::RPCClient;
 use gw_types::packed::Transaction;
 use std::path::Path;
 
-pub async fn dump_transaction<P: AsRef<Path>>(dir: P, rpc_client: &RPCClient, tx: Transaction) {
-    if let Err(err) = debugger::dump_transaction(dir, rpc_client, tx.clone()).await {
+pub async fn dump_transaction<P: AsRef<Path>>(dir: P, rpc_client: &RPCClient, tx: &Transaction) {
+    if let Err(err) = debugger::dump_transaction(dir, rpc_client, tx).await {
         log::error!(
-            "Faild to dump transaction {} error: {}",
+            "Failed to dump transaction {} error: {}",
             hex::encode(&tx.hash()),
             err
         );
