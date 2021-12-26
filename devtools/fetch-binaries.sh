6cf3c49b69896011045b2ede19016da90f5655f1
zero.qn <zero.qn@outlook.com>
2021-12-26 16:14:34 +0800

    chore(devtools): override state-validator from fixed prebuilds

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -5,6 +5,7 @@ docker pull nervos/godwoken-prebuilds:latest
 mkdir -p $dir_name && echo "Create dir"
 docker run --rm -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:latest cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
 # TODO: Wait prebuilds update
-docker pull ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.7.2-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
+docker pull ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.8-unlock-withdrawal-to-owner
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.8-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.8-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/state-validator /tmp/godwoken-scripts/state-validator && echo "Override state-validator"
 echo "Done"
