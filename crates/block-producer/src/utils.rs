d02ad8f8487597473088034f5ce3d221b79d7c8f
jjy <jjyruby@gmail.com>
2021-12-31 11:18:36 +0800

    fix: fix the parsing of query contract scripts

--- crates/block-producer/src/utils.rs
+++ crates/block-producer/src/utils.rs
@@ -1,19 +1,7 @@
 use crate::debugger;
-use anyhow::{anyhow, Result};
-use async_jsonrpc_client::Output;
 use gw_rpc_client::rpc_client::RPCClient;
 use gw_types::packed::Transaction;
-use serde::de::DeserializeOwned;
-use serde_json::from_value;
 use std::path::Path;
-
-// convert json output to result
-pub fn to_result<T: DeserializeOwned>(output: Output) -> Result<T> {
-    match output {
-        Output::Success(success) => Ok(from_value(success.result)?),
-        Output::Failure(failure) => Err(anyhow!("JSONRPC error: {}", failure.error)),
-    }
-}
 
 pub async fn dump_transaction<P: AsRef<Path>>(dir: P, rpc_client: &RPCClient, tx: Transaction) {
     if let Err(err) = debugger::dump_transaction(dir, rpc_client, tx.clone()).await {
