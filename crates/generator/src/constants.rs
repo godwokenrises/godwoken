e57c0fb454fbef7eea81b93bd1c1967a790b795d
Flouse <flouse@gmail.com>
2022-01-07 07:19:09 +0000

    Tune the max cycles of a layer2 transaction

--- crates/generator/src/constants.rs
+++ crates/generator/src/constants.rs
@@ -6,5 +6,5 @@ pub const MIN_DEPOSIT_CAPACITY: u64 = 1000_00000000;
 pub const MAX_WRITE_DATA_BYTES_LIMIT: usize = 25_000;
 // 2MB
 pub const MAX_READ_DATA_BYTES_LIMIT: usize = 1024 * 1024 * 2;
-// max cycles
-pub const L2TX_MAX_CYCLES: u64 = 7000_0000;
+/// max cycles of a layer2 transaction
+pub const L2TX_MAX_CYCLES: u64 = 100_000_000;
