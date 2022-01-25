62385e88bc77a27817b93a913ee648f1526d2d4d
jjy <jjyruby@gmail.com>
2022-01-25 23:31:54 +0800

    fix: move DepositInfo conversion to mem_block.rs

--- crates/types/src/conversion/mem_block.rs
+++ crates/types/src/conversion/mem_block.rs
@@ -99,10 +99,10 @@ impl_conversion_for_vector!(DepositInfo, DepositInfoVec, DepositInfoVecReader);
 impl_conversion_for_vector!(CellInfo, CellInfoVec, CellInfoVecReader);
 impl_conversion_for_vector!(SudtCustodian, SudtCustodianVec, SudtCustodianVecReader);
 impl_conversion_for_packed_iterator_pack!(WithdrawalRequestExtra, WithdrawalRequestExtraVec);
+impl_conversion_for_packed_iterator_pack!(DepositInfo, DepositInfoVec);
 impl_conversion_for_option!(H256, Byte32Opt, Byte32OptReader);
 impl_conversion_for_option!(
     CollectedCustodianCells,
     CollectedCustodianCellsOpt,
     CollectedCustodianCellsOptReader
 );
-impl_conversion_for_packed_iterator_pack!(DepositInfo, DepositInfoVec);
