ddfcfa86f7a78fa506e84d973b4ad72bbcce0f0d
jjy <jjyruby@gmail.com>
2022-01-26 19:03:39 +0800

    chore: update scripts binaries

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 dir_name='.tmp/binaries'
-docker pull nervos/godwoken-prebuilds:latest
+docker pull ghcr.io/jjyr/godwoken-prebuilds:compatible-feature
 [ -d $dir_name ] && rm -rf $dir_name && echo "Delete old dir"
 mkdir -p $dir_name && echo "Create dir"
-docker run --rm -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:latest cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
-docker run --rm -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:latest cp -r /scripts/godwoken-polyjuice /tmp && echo "Copy polyjuice"
-# TODO: Wait prebuilds update
-docker pull ghcr.io/zeroqn/godwoken-prebuilds:develop-feat--register-eth-eoa-mapping-on-deposit
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-feat--register-eth-eoa-mapping-on-deposit cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-feat--register-eth-eoa-mapping-on-deposit cp /scripts/godwoken-scripts/state-validator /tmp/godwoken-scripts/state-validator && echo "Override state-validator"
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-feat--register-eth-eoa-mapping-on-deposit cp -r /scripts/godwoken-polyjuice /tmp && echo "override polyjuice"
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/jjyr/godwoken-prebuilds:compatible-feature cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
 echo "Done"
