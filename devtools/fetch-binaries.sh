e646d8a1e74d3fda039f4ae3e03b4a361ea3a1ba
jjy <jjyruby@gmail.com>
2022-02-09 13:31:02 +0800

    fix: fix rec_id

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -4,4 +4,5 @@ docker pull ghcr.io/jjyr/godwoken-prebuilds:compatible-feature
 [ -d $dir_name ] && rm -rf $dir_name && echo "Delete old dir"
 mkdir -p $dir_name && echo "Create dir"
 docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/jjyr/godwoken-prebuilds:compatible-feature cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/jjyr/godwoken-prebuilds:compatible-feature cp -r /scripts/godwoken-polyjuice /tmp && echo "Copy polyjuice"
 echo "Done"
