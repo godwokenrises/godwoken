7cf6372946318b273c4a2d1f1b0a113e34383a5e
zero.qn <zero.qn@outlook.com>
2022-03-03 15:28:03 +0800

    chore(devtools): corresponding godwoken-scripts binaries

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dir_name='.tmp/binaries'
-docker pull ghcr.io/zeroqn/godwoken-prebuilds:develop-chore-rollup-config-allowed-type-hash-id
+docker pull ghcr.io/zeroqn/godwoken-prebuilds:develop-fix-secp256k1-witness-limit
 [ -d $dir_name ] && rm -rf $dir_name && echo "Delete old dir"
 mkdir -p $dir_name && echo "Create dir"
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-chore-rollup-config-allowed-type-hash-id cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-chore-rollup-config-allowed-type-hash-id cp -r /scripts/godwoken-polyjuice /tmp && echo "Copy polyjuice"
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-fix-secp256k1-witness-limit cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:develop-fix-secp256k1-witness-limit cp -r /scripts/godwoken-polyjuice /tmp && echo "Copy polyjuice"
 echo "Done"
