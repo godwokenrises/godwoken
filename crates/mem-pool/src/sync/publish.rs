bce5d5ad7f3e2ceca8988651cd3f1f96a6723c6d
Yin Guanhao <sopium@mysterious.site>
2022-03-25 13:13:51 +0800

    feat: mem pool p2p sync

--- crates/mem-pool/src/sync/publish.rs
+++ crates/mem-pool/src/sync/publish.rs
@@ -1,3 +1,4 @@
+use gw_common::H256;
 use gw_types::{
     offchain::DepositInfo,
     packed::{
@@ -8,25 +9,52 @@ use gw_types::{
 };
 use tokio::sync::mpsc::{Receiver, Sender};
 
-use super::mq::{tokio_kafka, Produce};
+use super::{
+    mq::{tokio_kafka, Produce},
+    p2p,
+};
 
 const CHANNEL_BUFFER_SIZE: usize = 1000;
 pub(crate) struct PublishMemPoolActor {
-    receiver: Receiver<RefreshMemBlockMessageUnion>,
-    producer: tokio_kafka::Producer,
+    receiver: Receiver<NewTipOrMessage>,
+    producer: Option<tokio_kafka::Producer>,
+    p2p_publisher: Option<p2p::Publisher>,
 }
 
 impl PublishMemPoolActor {
     pub(crate) fn new(
-        receiver: Receiver<RefreshMemBlockMessageUnion>,
-        producer: tokio_kafka::Producer,
+        receiver: Receiver<NewTipOrMessage>,
+        producer: Option<tokio_kafka::Producer>,
+        p2p_publisher: Option<p2p::Publisher>,
     ) -> Self {
-        Self { receiver, producer }
+        Self {
+            receiver,
+            producer,
+            p2p_publisher,
+        }
     }
 
-    async fn handle(&mut self, msg: RefreshMemBlockMessageUnion) {
-        if let Err(err) = self.producer.send(msg).await {
-            log::error!("[Fan out mem block] message failed: {:?}", err);
+    async fn handle(&mut self, msg: NewTipOrMessage) {
+        match msg {
+            NewTipOrMessage::Message(msg) => {
+                if let Some(p) = self.p2p_publisher.as_mut() {
+                    p.publish(msg.clone()).await;
+                }
+
+                if let Some(p) = self.producer.as_mut() {
+                    if let Err(err) = p.send(msg).await {
+                        log::error!("[Fan out mem block] message failed: {:?}", err);
+                    }
+                }
+            }
+            NewTipOrMessage::NewTip(new_tip) => {
+                if let Some(p) = self.p2p_publisher.as_mut() {
+                    p.handle_new_tip(new_tip).await;
+                }
+            }
+            NewTipOrMessage::SetP2PPublisher(p2p_publisher) => {
+                self.p2p_publisher = Some(p2p_publisher);
+            }
         }
     }
 }
@@ -38,17 +66,32 @@ async fn publish_handle(mut actor: PublishMemPoolActor) {
     }
 }
 
+pub(crate) enum NewTipOrMessage {
+    NewTip((H256, u64)),
+    Message(RefreshMemBlockMessageUnion),
+    SetP2PPublisher(p2p::Publisher),
+}
+
 pub(crate) struct MemPoolPublishService {
-    sender: Sender<RefreshMemBlockMessageUnion>,
+    sender: Sender<NewTipOrMessage>,
 }
 
 impl MemPoolPublishService {
-    pub(crate) fn start(producer: tokio_kafka::Producer) -> Self {
+    pub(crate) fn start(
+        producer: Option<tokio_kafka::Producer>,
+        p2p_publisher: Option<p2p::Publisher>,
+    ) -> Self {
         let (sender, receiver) = tokio::sync::mpsc::channel(CHANNEL_BUFFER_SIZE);
 
-        let actor = PublishMemPoolActor::new(receiver, producer);
+        let actor = PublishMemPoolActor::new(receiver, producer, p2p_publisher);
         tokio::spawn(publish_handle(actor));
         Self { sender }
+    }
+
+    pub(crate) async fn new_tip(&self, new_tip: (H256, u64)) {
+        if let Err(err) = self.sender.send(NewTipOrMessage::NewTip(new_tip)).await {
+            log::error!("Send new tip message with error: {}", err);
+        }
     }
 
     pub(crate) async fn new_tx(&self, tx: L2Transaction, current_tip_block_number: u64) {
@@ -58,10 +101,12 @@ impl MemPoolPublishService {
             .build();
         if let Err(err) = self
             .sender
-            .send(RefreshMemBlockMessageUnion::NextL2Transaction(next_tx))
+            .send(NewTipOrMessage::Message(
+                RefreshMemBlockMessageUnion::NextL2Transaction(next_tx),
+            ))
             .await
         {
-            log::error!("Send new tx message with error: {:?}", err);
+            log::error!("Send new tx message with error: {}", err);
         }
     }
 
@@ -76,9 +121,20 @@ impl MemPoolPublishService {
             .withdrawals(withdrawals.pack())
             .deposits(deposits.pack())
             .build();
-        let msg = RefreshMemBlockMessageUnion::NextMemBlock(next_mem_block);
+        let msg =
+            NewTipOrMessage::Message(RefreshMemBlockMessageUnion::NextMemBlock(next_mem_block));
         if let Err(err) = self.sender.send(msg).await {
-            log::error!("Send mem block message with error: {:?}", err);
+            log::error!("Send mem block message with error: {}", err);
+        }
+    }
+
+    pub(crate) async fn set_p2p_publisher(&self, p2p_publisher: p2p::Publisher) {
+        if let Err(err) = self
+            .sender
+            .send(NewTipOrMessage::SetP2PPublisher(p2p_publisher))
+            .await
+        {
+            log::error!("Send set p2p publisher error: {}", err);
         }
     }
 }
