71d3f44761516b9d146fe5363dc2eafbc4ff27e2
Flouse <flouse@gmail.com>
2022-03-27 08:25:21 +0000

    fix(gw-tools): Use the first Godwoken account (id = 2) as block_producer by default

--- crates/tools/src/generate_config.rs
+++ crates/tools/src/generate_config.rs
@@ -68,7 +68,7 @@ pub async fn generate_node_config(args: GenerateNodeConfigArgs<'_>) -> Result<Co
         .into();
 
     // build configuration
-    let account_id = 0;
+    let account_id = 2; // Use the first Godwoken account (id = 2) as block_producer by default
     let node_wallet_info = get_wallet_info(privkey_path);
     let code_hash: [u8; 32] = {
         let mut hash = [0u8; 32];
