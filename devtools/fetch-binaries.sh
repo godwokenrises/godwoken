bad7f2e933da5fce99b6b591e93e69be40399bfb
zero.qn <zero.qn@outlook.com>
2022-04-07 18:14:09 +0800

    chore(devtools): switch to feature prebuilds

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -6,7 +6,8 @@ docker pull nervos/godwoken-prebuilds:$GODWOKEN_TAG
 mkdir -p $dir_name && echo "Create dir"
 docker run --rm -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:$GODWOKEN_TAG cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
 # TODO: Wait prebuilds update
-docker pull ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.8-unlock-withdrawal-to-owner
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.8-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:docker-publish-v0.8-unlock-withdrawal-to-owner cp /scripts/godwoken-scripts/state-validator /tmp/godwoken-scripts/state-validator && echo "Override state-validator"
+docker pull ghcr.io/zeroqn/godwoken-prebuilds:dev-feat-fast-withdrawal-to-v1
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:dev-feat-fast-withdrawal-to-v1 cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
+docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:dev-feat-fast-withdrawal-to-v1 cp /scripts/godwoken-scripts/state-validator /tmp/godwoken-scripts/state-validator && echo "Override state-validator"
 echo "Done"
+
