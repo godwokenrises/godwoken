1f5ad7cd98b28886f07e91022355e28a9e2ebed4
zeroqn <zero.qn@outlook.com>
2022-04-21 01:31:17 +0800

    fix(tools): parse account from str failure (#663)
    Hex isn't decoded

--- crates/tools/src/account.rs
+++ crates/tools/src/account.rs
@@ -87,7 +87,7 @@ pub async fn parse_account_from_str(
 ) -> Result<H256> {
     // if match script hash
     if account.starts_with("0x") && account.len() == 66 {
-        let r = H256::from_slice(account[2..].as_bytes())?;
+        let r = H256::from_slice(&hex::decode(&account[2..].as_bytes())?)?;
         return Ok(r);
     }
 
