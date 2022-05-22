614d92ecedc51ad890d72c3b8fe068a16dad830b
jjy <jjyruby@gmail.com>
2022-05-22 10:59:07 +0800

    chore: update MIN_CKB_DEPOSIT_CAPACITY and MIN_SUDT_DEPOSIT_CAPACITY

--- crates/mem-pool/src/constants.rs
+++ crates/mem-pool/src/constants.rs
@@ -5,8 +5,8 @@ pub const MAX_MEM_BLOCK_WITHDRAWALS: usize = 50;
 /// MAX withdrawals in the mem block
 pub const MAX_MEM_BLOCK_TXS: usize = 1000;
 /// MIN CKB deposit capacity, calculated from custodian cell size
-pub const MIN_CKB_DEPOSIT_CAPACITY: u64 = 290_00000000;
+pub const MIN_CKB_DEPOSIT_CAPACITY: u64 = 298_00000000;
 /// MIN Simple UDT deposit capacity, calculated from custodian cell size + simple UDT script
-pub const MIN_SUDT_DEPOSIT_CAPACITY: u64 = 371_00000000;
+pub const MIN_SUDT_DEPOSIT_CAPACITY: u64 = 379_00000000;
 /// MAX custodian cells
 pub const MAX_CUSTODIANS: usize = 50;
