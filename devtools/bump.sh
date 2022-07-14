1e3f36d2ad9739d9b6187daaad79cb9ebe5c4139
jjy <jjyruby@gmail.com>
2022-07-14 14:14:39 +0800

    chore: Add devtools/bump.sh

---
+++ devtools/bump.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+[ -n "${DEBUG:-}" ] && set -x || true
+
+main() {
+  if [ $# != 1 ]; then
+    echo "bump.sh version" >&2
+    exit 1
+  fi
+  local v="$1"
+  find . -name 'Cargo.toml' -print0 | xargs -0 sed -i.bak \
+    -e 's/^version = .*/version = "'"$v"'"/' \
+    -e 's/\({.*path = ".*",.* version = "= \)[^"]*/\1'"$v"'/'
+  find . -name 'Cargo.toml.bak' -exec rm -f {} \;
+  cargo check
+}
+
+main "$@"
