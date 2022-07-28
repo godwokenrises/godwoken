30bd29c5c5b49c3235ec7b195bdea37cc4da49bb
Yin Guanhao <sopium@mysterious.site>
2022-07-28 10:57:43 +0800

    Add RPC get_pending_tx_hashes

--- crates/store/src/snapshot.rs
+++ crates/store/src/snapshot.rs
@@ -1,4 +1,7 @@
-use gw_db::{schema::Col, RocksDBSnapshot};
+use gw_db::{
+    schema::{Col, COLUMN_MEM_POOL_TRANSACTION},
+    DBIterator, RocksDBSnapshot,
+};
 
 use crate::traits::{chain_store::ChainStore, kv_store::KVStoreRead};
 
@@ -20,5 +23,14 @@ impl KVStoreRead for StoreSnapshot {
             .get_pinned(col, key)
             .expect("db operation should be ok")
             .map(|v| Box::<[u8]>::from(v.as_ref()))
+    }
+}
+
+impl StoreSnapshot {
+    pub fn iter_mem_pool_transactions(&self) -> impl Iterator<Item = Box<[u8]>> + '_ {
+        self.inner
+            .iter(COLUMN_MEM_POOL_TRANSACTION, gw_db::IteratorMode::Start)
+            .expect("db read should not fail")
+            .map(|(k, _)| k)
     }
 }
