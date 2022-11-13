39b2cb88c38cb0198296342bdc008d4cb95088b6
keroro <keroroxx520@gmail.com>
2022-11-13 22:22:33 +0800

    feat(utils): add fork_config into RollupContext

--- crates/utils/src/rollup_context.rs
+++ crates/utils/src/rollup_context.rs
@@ -1,3 +1,4 @@
+use gw_config::ForkConfig;
 use gw_types::core::H256;
 use gw_types::{packed::RollupConfig, prelude::Unpack};
 
@@ -7,6 +8,7 @@ pub struct RollupContext {
     /// rollup_state_cell's type hash
     pub rollup_script_hash: H256,
     pub rollup_config: RollupConfig,
+    pub fork_config: ForkConfig,
 }
 
 impl RollupContext {
