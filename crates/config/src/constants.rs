a62142ee77f56ea795cf7267433b4d00beb699a0
jjy <jjyruby@gmail.com>
2022-11-17 10:11:17 +0800

    chore: rename constant

--- crates/config/src/constants.rs
+++ crates/config/src/constants.rs
@@ -3,9 +3,9 @@ pub const MAX_TX_SIZE: usize = 50_000;
 /// MAX withdrawal size 50 KB
 pub const MAX_WITHDRAWAL_SIZE: usize = 50_000;
 // 25 KB
-pub const MAX_WRITE_DATA_BYTES_LIMIT: usize = 25 * 1024;
+pub const MAX_WRITE_DATA_BYTES: usize = 25 * 1024;
 // 2MB
-pub const MAX_TOTAL_READ_DATA_BYTES_LIMIT: usize = 1024 * 1024 * 2;
+pub const MAX_TOTAL_READ_DATA_BYTES: usize = 1024 * 1024 * 2;
 /// Max cycles of a layer2 transaction
 pub const L2TX_MAX_CYCLES_150M: u64 = 150_000_000;
 /// Max cycles of a layer2 transaction
