5ffbeb48689856760a976d9154d3c31bf31f61c0
keroro520 <keroroxx520@gmail.com>
2022-11-18 16:48:15 +0800

    feat: determine global state version according to fork height (#858)

--- crates/utils/src/rollup_context.rs
+++ crates/utils/src/rollup_context.rs
@@ -1,6 +1,7 @@
 use gw_config::ForkConfig;
 use gw_types::core::H256;
-use gw_types::{packed::RollupConfig, prelude::Unpack};
+use gw_types::packed::RollupConfig;
+use gw_types::prelude::Unpack;
 
 /// A wildly used context, contains several common-used configurations.
 #[derive(Clone)]
@@ -12,6 +13,11 @@ pub struct RollupContext {
 }
 
 impl RollupContext {
+    /// Returns the version of global state for `block_number`.
+    pub fn global_state_version(&self, block_number: u64) -> u8 {
+        self.fork_config.global_state_version(block_number)
+    }
+
     pub fn last_finalized_block_number(&self, tip_number: u64) -> u64 {
         let finality: u64 = self.rollup_config.finality_blocks().unpack();
         tip_number.saturating_sub(finality)
