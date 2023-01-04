ca0874cd246d7a175340eedce94f241cc649539b
magicalne <magicalne@gmail.com>
2023-01-04 15:16:21 +0800

    fix: big_mod_exp could be overflow

--- gwos-evm/c/contracts.h
+++ gwos-evm/c/contracts.h
@@ -326,18 +326,37 @@ int big_mod_exp_required_gas(const uint8_t* input, const size_t input_size,
   size_t base_gas = 0;
   uint128_t gas = 0;
 
-  const size_t content_size = base_size + exp_size + mod_size;
-  const size_t copy_size = input_size > content_size + 96
+  size_t content_size = 0;
+  size_t content_extra_size = 0;
+  if (__builtin_uaddl_overflow(base_size, exp_size, &content_size) ||
+      __builtin_uaddl_overflow(content_size, mod_size, &content_size) ||
+      __builtin_uaddl_overflow(content_size, 96, &content_extra_size)) {
+    mbedtls_mpi_free(&base_len);
+    mbedtls_mpi_free(&exp_len);
+    mbedtls_mpi_free(&mod_len);
+
+    mbedtls_mpi_free(&exp_head);
+    mbedtls_mpi_free(&adj_exp_len);
+    mbedtls_mpi_free(&gas_big);
+    return ERROR_MOD_EXP;
+  }
+  
+  const size_t copy_size = input_size > content_extra_size 
     ? content_size
     : (input_size > 96 ? input_size - 96 : 0);
-  uint8_t *content = (uint8_t*)malloc(content_size);
+  // `copy_size` <= input_size - 96 and input_size <= 132kb
+  uint8_t *content = (uint8_t*)malloc(copy_size);
   if (content == NULL) {
     return_value = FATAL_PRECOMPILED_CONTRACTS;
     goto mod_exp_gas_cleanup;
   }
-  memset(content, 0, content_size);
+  memset(content, 0, copy_size);
   memcpy(content, input + 96, copy_size);
 
+  if (base_size > copy_size) {
+    return_value = ERROR_MOD_EXP;
+    goto mod_exp_gas_cleanup;
+  }
   ret = mbedtls_mpi_read_binary(&exp_head, content + base_size, exp_head_size);
   if (ret != 0) {
     return_value = ERROR_MOD_EXP;
@@ -450,26 +469,52 @@ int big_mod_exp(gw_context_t* ctx,
   mbedtls_mpi_init(&mod);
   mbedtls_mpi_init(&result);
 
-  const size_t content_size = base_size + exp_size + mod_size;
-  const size_t copy_size = input_size > content_size + 96
+  size_t content_size = 0;
+  size_t content_extra_size = 0;
+  if (__builtin_uaddl_overflow(base_size, exp_size, &content_size) ||
+      __builtin_uaddl_overflow(content_size, mod_size, &content_size) ||
+      __builtin_uaddl_overflow(content_size, 96, &content_extra_size)) {
+    mbedtls_mpi_free(&base_len);
+    mbedtls_mpi_free(&exp_len);
+    mbedtls_mpi_free(&mod_len);
+    mbedtls_mpi_free(&base);
+    mbedtls_mpi_free(&exp);
+    mbedtls_mpi_free(&mod);
+    mbedtls_mpi_free(&result);
+
+    return ERROR_MOD_EXP;
+  }
+  const size_t copy_size = input_size > content_extra_size
     ? content_size
     : (input_size > 96 ? input_size - 96 : 0);
-  uint8_t *content = (uint8_t*)malloc(content_size);
+  // `copy_size` <= input_size - 96 and input_size <= 132kb
+  uint8_t *content = (uint8_t*)malloc(copy_size);
   if (content == NULL) {
     return_value = FATAL_PRECOMPILED_CONTRACTS;
     goto mod_exp_cleanup;
   }
-  memset(content, 0, content_size);
+  memset(content, 0, copy_size);
   memcpy(content, input_src + 96, copy_size);
 
-
+  if (base_size > copy_size) {
+    return_value = ERROR_MOD_EXP;
+    goto mod_exp_cleanup;
+  }
   ret = mbedtls_mpi_read_binary(&base, content, base_size);
   if (ret != 0) {
     return_value = ERROR_MOD_EXP;
     goto mod_exp_cleanup;
   }
+  if (exp_size > copy_size - base_size) {
+    return_value = ERROR_MOD_EXP;
+    goto mod_exp_cleanup;
+  }
   ret = mbedtls_mpi_read_binary(&exp, content + base_size, exp_size);
   if (ret != 0) {
+    return_value = ERROR_MOD_EXP;
+    goto mod_exp_cleanup;
+  }
+  if (mod_size > copy_size - base_size - exp_size) {
     return_value = ERROR_MOD_EXP;
     goto mod_exp_cleanup;
   }
