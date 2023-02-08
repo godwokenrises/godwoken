45fb0f0a07e69cead6ae74c3f6f16ab5ed0559a7
jjy <jjyruby@gmail.com>
2023-02-08 17:53:22 +0800

    chore: bump monorepo version to 1.12.0-rc1

--- devtools/bump.sh
+++ devtools/bump.sh
@@ -10,11 +10,21 @@ main() {
     exit 1
   fi
   local v="$1"
-  find ./crates ./gwos/crates -name 'Cargo.toml' -print0 | xargs -0 sed -i.bak \
+
+  # Update the version in Cargo.toml files
+  find ./crates ./gwos/crates ./web3 -name 'Cargo.toml' ! -regex './crates/autorocks/.*' -print0 | xargs -0 sed -i.bak \
     -e 's/^version = .*/version = "'"$v"'"/' \
-    -e 's/\({.*path = ".*",.* version = "= \)[^"]*/\1'"$v"'/'
+    -e '/autorocks/! s/\({.*path = ".*",.* version = "= \)[^"]*/\1'"$v"'/'
   find . -name 'Cargo.toml.bak' -exec rm -f {} \;
   cargo check
+  cd web3 && cargo check && cd ..
+
+  # Update the version in package.json files
+  find ./web3 -name 'package.json' -print0 | xargs -0 sed -i.bak \
+    -e 's/^\([[:blank:]]*\)"version": .*,/\1"version": "'"$v"'",/' \
+    -e 's|^\([[:blank:]]*\)"@godwoken-web3/godwoken": .*,|\1"@godwoken-web3/godwoken": "'"$v"'",|'
+  find . -name 'package.json.bak' -exec rm -f {} \;
+  cd web3 && yarn upgrade -P godwoken-web3 --frozen-lockfile && cd ..
 }
 
 main "$@"
