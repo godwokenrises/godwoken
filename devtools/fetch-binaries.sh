9062bfab4cc4ed983c820c598b41d99816801c0b
Yin Guanhao <sopium@mysterious.site>
2023-07-19 15:52:36 +0800

    Update devtools/fetch-binaries.sh

--- devtools/fetch-binaries.sh
+++ devtools/fetch-binaries.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 dir_name='.tmp/binaries'
-GODWOKEN_TAG=v0.10.3
-docker pull nervos/godwoken-prebuilds:$GODWOKEN_TAG
+IMAGE=ghcr.io/nervosnetwork/godwoken-prebuilds:v0.10.7
+docker pull $IMAGE
 [ -d $dir_name ] && rm -rf $dir_name && echo "Delete old dir"
 mkdir -p $dir_name && echo "Create dir"
-docker run --rm -v $(pwd)/$dir_name:/tmp nervos/godwoken-prebuilds:$GODWOKEN_TAG cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
-# TODO: Wait prebuilds update
-docker pull ghcr.io/zeroqn/godwoken-prebuilds:dev-feat-fast-withdrawal-to-v1
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:dev-feat-fast-withdrawal-to-v1 cp /scripts/godwoken-scripts/withdrawal-lock /tmp/godwoken-scripts/withdrawal-lock && echo "Override withdrawal-lock"
-docker run --rm -v $(pwd)/$dir_name:/tmp ghcr.io/zeroqn/godwoken-prebuilds:dev-feat-fast-withdrawal-to-v1 cp /scripts/godwoken-scripts/state-validator /tmp/godwoken-scripts/state-validator && echo "Override state-validator"
+docker run --rm -v $(pwd)/$dir_name:/tmp $IMAGE cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
 echo "Done"
-
